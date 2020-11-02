21e3441ea90b88c7454920419908f255cd481099
RXCodes <61912060+RXCodes@users.noreply.github.com>
2020-11-02 10:07:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
-var filter = require('./chat filter.js');
+var filter = require('./chatfilter');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
