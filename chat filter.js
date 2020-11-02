b3f1794526d5a77178953f1f18952265a0c9328c
RXCodes <61912060+RXCodes@users.noreply.github.com>
2020-11-02 10:02:34 -0500

    Create chat filter.js

---
+++ chat filter.js
@@ -0,0 +1,18 @@
+function chat(message, check) {
+
+  var filter = message; 
+  
+  var banned = JSON.parse(check);
+  for (i = 0; i < banned.length; i++) {
+    var count = "";
+    var word = banned[i];
+    for (x = 0; x < word.length; x++) {
+count = count + "*";
+}
+    var re = new RegExp(banned[i],"gi");
+      var filter = filter.replace(re,count);
+      filter = filter.replace(banned[i],count);
+  }
+  return filter;
+
+}
