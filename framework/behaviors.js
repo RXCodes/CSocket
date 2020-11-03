1423e5d8d217f4319e3c03038e375b4bc3512ba6
RXCodes <61912060+RXCodes@users.noreply.github.com>
2020-11-03 01:01:16 -0500

    Update behaviors.js

--- framework/behaviors.js
+++ framework/behaviors.js
@@ -1,4 +1,11 @@
-// Behavior: Modify Dictionary Key
+// Behavior | Modify Dictionary Key
+// Returns => dictionary: dictionary, boolean: successfully executed
 exports.modify_dictionary_key = function (dictionary, key, value) {
-  
+  try {
+    JSON.parse(dictionary);
+  } catch (e) {
+    return [dictionary, false];
+  }
+  var dict = JSON.parse(dictionary);
+  dict[key] = value;
 };
