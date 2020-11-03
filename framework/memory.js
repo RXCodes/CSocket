8157dac4850dbcd96db4150b3c9999ad2f0764b8
RXCodes <61912060+RXCodes@users.noreply.github.com>
2020-11-03 04:16:42 -0500

    Update memory.js

--- framework/memory.js
+++ framework/memory.js
@@ -1,1 +1,32 @@
-exports.store = function (variable, ) {
+// global storage object
+var storage = {};
+
+// store a global variable
+exports.store = function (variable, value) {
+  try {
+    let storage[variable] = value;
+  } catch (e) {
+    return false;
+	}
+	storage[variable] = value;
+	return true;
+}
+
+// retrieve a global variable
+exports.retrieve = function (variable, default_value) {
+  try {
+    let test = storage[variable];
+  } catch (e) {
+    return default_value;
+	}
+	return storage[variable];
+}
+
+// delete a global variable
+exports.delete = function (variable) {
+  if (storage.hasOwnProperty(variable)) {
+	delete storage[variable];
+	return true;
+	} else
+		return false;
+}
