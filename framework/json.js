9e0652a3707a1e8725162247e5cebb38f3d4c9e9
RXCodes <61912060+RXCodes@users.noreply.github.com>
2020-11-03 15:43:18 -0500

    Update json.js

--- framework/json.js
+++ framework/json.js
@@ -2,14 +2,14 @@
 // Returns => JSON: dictionary, boolean: success
 exports.modify_dictionary_key = function (dictionary, key, value) {
   try {
-    let dict = JSON.parse(dictionary);
+    let dict = dictionary;
     dict[key] = value;
     return [dict, true];
     
   } catch (e) {
     return [dictionary, false];
   }
-  var dict = JSON.parse(dictionary);
+  var dict = dictionary;
   dict[key] = value;
   return [dict, true];
 };
@@ -18,7 +18,7 @@ exports.modify_dictionary_key = function (dictionary, key, value) {
 // Returns => string: value, boolean: success
 exports.get_dictionary_key = function (dictionary, key, default_value) {
   try {
-    let dict = JSON.parse(dictionary);
+    let dict = dictionary;
     return [dict[key], true];
   } catch (e) {
     return [default_value, false];
@@ -31,7 +31,7 @@ exports.get_dictionary_key = function (dictionary, key, default_value) {
 // Returns => JSON: dictionary, boolean: success
 exports.delete_dictionary_key = function (dictionary, key) {
   try {
-      let dict = JSON.parse(dictionary);
+      let dict = dictionary;
   if (dict.hasOwnProperty(key)) {
     delete dict[key];
     return [dict, true];
@@ -42,7 +42,7 @@ else {
   } catch (e) {
     return [dictionary, false];
   }
-  var dict = JSON.parse(dictionary);
+  var dict = dictionary;
   if (dict.hasOwnProperty(key)) {
     delete dict[key];
     return [dict, true];
@@ -57,7 +57,7 @@ return [dictionary, false];
 // SIDENOTE: Works for both arrays and dictionaries!
 exports.test_dictionary_key = function (dictionary, key) {
   try {
-    let dict = JSON.parse(dictionary);
+    let dict = dictionary;
     if (dict.hasOwnProperty(key)) {
       return [true, true];
     }
@@ -67,7 +67,7 @@ exports.test_dictionary_key = function (dictionary, key) {
   } catch (e) {
     return [false, false];
   }
-  var dict = JSON.parse(dictionary);
+  var dict = dictionary;
   if (dict.hasOwnProperty(key)) {
     return [true, true];
   }
@@ -80,7 +80,7 @@ return [false, true];
 // Returns => JSON: array / dictionary, boolean: success
 exports.clear_json = function (dictionary) {
   try {
-let dict = JSON.parse(dictionary);
+let dict = dictionary;
   for (var remove in dict) {
     if (dict.hasOwnProperty(remove)) {
         delete dict[remove];
@@ -89,15 +89,15 @@ let dict = JSON.parse(dictionary);
 } catch (e) {
     return [false];
   }
-  var dict = JSON.parse(dictionary);
+  var dict = dictionary;
   for (var remove in dict) {
     if (dict.hasOwnProperty(remove)) {
         delete dict[remove];
     }
-}
+  }
 };
 
-// Set JSON
+// Set JSON from string
 // Returns => JSON: dictionary / array, boolean: success
 exports.set_json = function (dictionary) {
   try {
@@ -114,7 +114,7 @@ exports.set_json = function (dictionary) {
 // Returns => JSON: array, boolean: success
 exports.append_value = function (array, value) {
   try {
-    let dict = JSON.parse(array);
+    let dict = array;
   dict.push(value);
   return [dict, true];
   } catch (e) {
@@ -130,13 +130,13 @@ exports.append_value = function (array, value) {
 // Returns => JSON: array, boolean: success
 exports.insert_value = function (array, value, index) {
   try {
-    let dict = JSON.parse(array);
+    let dict = array;
   dict.splice(index, 0, value);
   return [dict, true];
   } catch (e) {
     return [array, false];
   }
-  var dict = JSON.parse(array);
+  var dict = array;
   dict.splice(index, 0, value);
   return [dict, true];
 
@@ -146,13 +146,13 @@ exports.insert_value = function (array, value, index) {
 // Returns => JSON: array, boolean: success
 exports.replace_value = function (array, value, index) {
   try {
-    let dict = JSON.parse(array);
+    let dict = array;
   dict.splice(index, 1, value);
   return [dict, true];
   } catch (e) {
     return [array, false];
   }
-  var dict = JSON.parse(array);
+  var dict = array;
   dict.splice(index, 1, value);
   return [dict, true];
 
@@ -162,13 +162,13 @@ exports.replace_value = function (array, value, index) {
 // Returns => JSON: array, boolean: success
 exports.sort_array = function (array) {
   try {
-    let dict = JSON.parse(array);
+    let dict = array;
   dict.sort();
   return [dict, true];
   } catch (e) {
     return [array, false];
   }
-  var dict = JSON.parse(array);
+  var dict = array;
   dict.sort();
   return [dict, true];
 
@@ -178,13 +178,13 @@ exports.sort_array = function (array) {
 // Returns => JSON: array, boolean: success
 exports.reverse_array = function (array) {
   try {
-    let dict = JSON.parse(array);
+    let dict = array;
   dict.reverse();
   return [dict, true];
   } catch (e) {
     return [array, false];
   }
-  var dict = JSON.parse(array);
+  var dict = array;
   dict.reverse();
   return [dict, true];
 
@@ -194,13 +194,13 @@ exports.reverse_array = function (array) {
 // Returns => JSON: array, boolean: success
 exports.ascending_numeric_sort_array = function (array) {
   try {
-    let dict = JSON.parse(array);
+    let dict = array;
   dict.sort(function(a, b){return a - b});
   return [dict, true];
   } catch (e) {
     return [array, false];
   }
-  var dict = JSON.parse(array);
+  var dict = array;
   dict.sort(function(a, b){return a - b});
   return [dict, true];
 
@@ -210,13 +210,13 @@ exports.ascending_numeric_sort_array = function (array) {
 // Returns => JSON: array, boolean: success
 exports.descending_numeric_sort_array = function (array) {
   try {
-    let dict = JSON.parse(array);
+    let dict = array;
   dict.sort(function(a, b){return b - a});
   return [dict, true];
   } catch (e) {
     return [array, false];
   }
-  var dict = JSON.parse(array);
+  var dict = array;
   dict.sort(function(a, b){return b - a});
   return [dict, true];
 
@@ -226,13 +226,13 @@ exports.descending_numeric_sort_array = function (array) {
 // Returns => JSON: array, boolean: success
 exports.random_sort_array = function (array) {
   try {
-    let dict = JSON.parse(array);
+    let dict = array;
   dict.sort(function(a, b){return 0.5 - Math.random()});
   return [dict, true];
   } catch (e) {
     return [array, false];
   }
-  var dict = JSON.parse(array);
+  var dict = array;
   dict.sort(function(a, b){return 0.5 - Math.random()});
   return [dict, true];
 
@@ -242,13 +242,13 @@ exports.random_sort_array = function (array) {
 // Returns => JSON: array, boolean: success
 exports.get_keys = function (dictionary) {
   try {
-    let dict = JSON.parse(dictionary);
+    let dict = dictionary;
   Object.keys(dict);
   return [dict, true];
   } catch (e) {
     return [array, false];
   }
-  var dict = JSON.parse(array);
+  var dict = dictionary;
   Object.keys(dict);
   return [dict, true];
 
@@ -258,13 +258,13 @@ exports.get_keys = function (dictionary) {
 // Returns => JSON: array, boolean: success
 exports.pop = function (array) {
   try {
-    let dict = JSON.parse(array);
+    let dict = array;
   dict.pop();
   return [dict, true];
   } catch (e) {
     return [array, false];
   }
-  var dict = JSON.parse(array);
+  var dict = array;
   dict.pop();
   return [dict, true];
 
@@ -274,14 +274,14 @@ exports.pop = function (array) {
 // Returns => JSON: array, boolean: success
 exports.delete_index = function (array, index) {
   try {
-    let dict = JSON.parse(array);
+    let dict = array;
   dict.splice(index, array);
 
   return [dict, true];
   } catch (e) {
     return [array, false];
   }
-  var dict = JSON.parse(array);
+  var dict = array;
   dict.splice(index, array);
   return [dict, true];
 
@@ -291,12 +291,12 @@ exports.delete_index = function (array, index) {
 // Returns => integer: count, boolean: success
 exports.count = function (array) {
   try {
-    let dict = JSON.parse(array);
+    let dict = array;
   return [dict.length(), true];
   } catch (e) {
     return [0, false];
   }
-  var dict = JSON.parse(array);
+  var dict = array;
   return [dict.length(), true];
 
 };
@@ -305,12 +305,12 @@ exports.count = function (array) {
 // Returns => string: value, boolean: success
 exports.get_array_value = function (array, index, default_value) {
   try {
-    let dict = JSON.parse(array);
+    let dict = array;
   return [dict[index], true];
   } catch (e) {
     return [default_value, true];
   }
-  var dict = JSON.parse(array);
+  var dict = array;
   return [dict[index], true];
 
 };
@@ -319,12 +319,12 @@ exports.get_array_value = function (array, index, default_value) {
 // Returns => string: value, boolean: success
 exports.get_first_array_value = function (array, default_value) {
   try {
-    let dict = JSON.parse(array);
+    let dict = array;
   return [dict[0], true];
   } catch (e) {
     return [default_value, true];
   }
-  var dict = JSON.parse(array);
+  var dict = array;
   return [dict[0], true];
 
 };
@@ -333,12 +333,12 @@ exports.get_first_array_value = function (array, default_value) {
 // Returns => string: value, boolean: success
 exports.get_first_array_value = function (array, default_value) {
   try {
-    let dict = JSON.parse(array);
+    let dict = array;
   return [dict[dict.length() - 1], true];
   } catch (e) {
     return [default_value, true];
   }
-  var dict = JSON.parse(array);
+  var dict = array;
   return [dict[dict.length() - 1], true];
 
 };
