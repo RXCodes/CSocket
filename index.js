0fb739e1d825a1d58551465c02caa9d5cbb6f92b
RXCodes <61912060+RXCodes@users.noreply.github.com>
2020-11-03 05:12:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ io.on('connection', function(socket) {
   
   socket.on('event', function(data) {
     let init = storage.retrieve("dictionary", {});
-    let behavior_output = json.modify_dictionary_key(init,"test","key");
+    let behavior_output = json.modify_dictionary_key(init,"test",data);
     storage.store("dictionary", behavior_output[0]);
     socket.emit('output', behavior_output[0]);
   });
@@ -26,6 +26,13 @@ io.on('connection', function(socket) {
   socket.on('event2', function(data) {
     let init = storage.retrieve("dictionary", {});
     let behavior_output = json.delete_dictionary_key(init,"test");
+    storage.store("dictionary", behavior_output[0]);
+    socket.emit('output', behavior_output[0]);
+  });
+  
+  socket.on('event3', function(data) {
+    let init = storage.retrieve("dictionary", {});
+    let behavior_output = json.get_dictionary_key(init,"test","none");
     storage.store("dictionary", behavior_output[0]);
     socket.emit('output', behavior_output[0]);
   });
